424702cb3a8a22f8b76d3f995daccc25e86ebe1e
akashamenon <akashamenon98@gmail.com>
2021-04-04 20:29:49 +0530

    First Express Program

---
+++ app.js
@@ -0,0 +1,20 @@
+var express=require('express')
+
+let app=express()
+
+app.get('/', (req,res)=>{
+
+    res.send("hello")
+
+}  )
+
+app.get('/home', (req,res)=>{
+
+    res.send("Welcome to my home page")
+
+} )
+
+app.listen(3000, ()=>{
+
+    console.log("Server started at http://localhost:3000/home")
+})
