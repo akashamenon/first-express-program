ec1c76962493fe70828eeb1f42fd0245eaf27760
akashamenon <akashamenon98@gmail.com>
2021-04-16 13:48:11 +0530

    update

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 var express=require('express')
+var bodyParser = require('body-parser')
 
 let app=express()
 
-app.get('/', (req,res)=>{
+app.use(bodyParser.urlencoded({extended: false}))
 
-    res.send("Hello World")
+app.use(bodyParser.json())
 
-}  )
 
-app.get('/home', (req,res)=>{
 
-    res.send("Welcome to my home page")
+app.get('/',(req,res)=>{
 
-} )
+    res.send("Welcome to my website")
+
+})
+
+
+
+
+app.post('/read', (req,res)=>{
+
+    var getName=req.body.getName
+    var getRoll=req.body.getRoll
+
+    res.json({"name" :getName,"rollno":getRoll} )
+})
+
+app.post('/add',(req,res)=>{
+    var getNum1=parseFloat(req.body.num1)
+    var getNum2=parseFloat(req.body.num2)
+
+    var x=getNum1 + getNum2
+
+    res.json({"sum":x})
+})
+
 
 app.listen(3000, ()=>{
-
-    console.log("Server started at http://localhost:3000/home")
+    console.log("Server started at http://localhost:3000")
 })
